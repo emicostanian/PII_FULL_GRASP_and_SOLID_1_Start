d24eaebe807ab2a185c5453fc6e7542eda176941
emicostanian <mariaemiliacostanian@gmail.com>
2023-09-11 09:23:17 -0300

    vuelvo a guardar por las dudas

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -6,12 +6,14 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic; // Agrega este using
+using System.Linq; // También necesitarás este using para el método Sum
 
 namespace Full_GRASP_And_SOLID.Library
 {
     public class Recipe
     {
-        private ArrayList steps = new ArrayList();
+        private List<Step> steps = new List<Step>(); // Cambia ArrayList a List<Step>
 
         public Product FinalProduct { get; set; }
 
@@ -25,14 +27,26 @@ namespace Full_GRASP_And_SOLID.Library
             this.steps.Remove(step);
         }
 
+        public double GetProductionCost()
+        {
+            double costInsumos = this.steps.Sum(step => step.Input.UnitCost * (step.Quantity/ 1000));
+            double costEquipamiento = this.steps.Sum(step => step.Equipment.HourlyCost * (step.Time / 60));
+            return costInsumos + costEquipamiento;
+        }
         public void PrintRecipe()
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
             foreach (Step step in this.steps)
             {
+                 
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+        
+        double productionCost = this.GetProductionCost(); // Calcula el costo total de producción.
+        Console.WriteLine($"Costo total de producción: ${productionCost}");
+            
         }
+
     }
 }
